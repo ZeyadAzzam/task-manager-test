6b92d71620a925246feebb199d73562ac487d36b
ZeyadAzzam <zoz341995@gmail.com>
2025-05-21 16:06:51 +0300

    update:swagger schema

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -15,14 +15,25 @@ const { createUser } = require("../controllers/userController");
  *         application/json:
  *           schema:
  *             type: object
+ *             required: [name, email, password]
  *             properties:
  *               name:
  *                 type: string
  *               email:
  *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *                 minLength: 8
  *     responses:
  *       201:
  *         description: User created successfully
+ *       400:
+ *         description: Validation error
+ *       409:
+ *         description: User with this email already exists
+ *       500:
+ *         description: Server error
  */
 router.post("/", createUser);
 

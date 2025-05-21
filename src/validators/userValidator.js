c35ac3dccf1d943bd5e7e56b981fce0e7949d978
ZeyadAzzam <zoz341995@gmail.com>
2025-05-21 16:07:17 +0300

    add password validation

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -3,5 +3,7 @@ const Joi = require("joi");
 exports.userSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
+  password: Joi.string()
+    .pattern(/^[a-zA-Z0-9]{8,30}$/)
+    .required(),
 });
